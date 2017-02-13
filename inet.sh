450b0b3bbace86ada74948f51a127e7de09ef54d
mark <w3rsin@outlook.com>
2017-02-13 23:08:16 +0100

    vpn support

--- inet.sh
+++ inet.sh
@@ -1,52 +1,69 @@
-#!/bin/bash
+#!/bin/zsh
 
 #user must be root to run this
-#if [ "$EUID" -ne 0 ]
-#    then echo "Must be root to run this"
-#    exit 1
-#fi
-if [ -z "$1" ]
+if [ "$EUID" -ne 0 ]
+    then echo "Must be root to run this"
+    exit 1
+fi
+if [ -z "$1" ]; then
     printf "specify the network\n"
     exit 1
 fi
 
-if [ "$1" -ne "uni" || "$1" -ne "home" ]
-    printf "unvalid network\n"
+#lrz support and start vpn
+#add more options if needed
+if [ "$1" = "uni" ]; then
+    #specify lrz or eduroam and start respective services
+    printf "uni-network\n"
+    network="eduoram"
+elif [ "$1" = "home" ]; then
+    printf "home-network\n"
+    network="home"
+elif [ "$1" = "lrz" ]
+    printf "lrz-network: will start vpn services\n"
+    network="lrz"
+else
+    printf "no such network\n"
     exit 1
 fi
+
 interface="wlp3s0"
 
-#ip link set $interface up
+ip link set $interface up
 printf "Interface is up.\n"
 
 #get pid of wpa_supplicant
 wpa_pid="$(ps aux | pgrep wpa)"
 
-#TODO: check for arguments like uni, home... and run respective wpa script
 #check if it is running if it is, kill it
-#-n returns if a var is set or not empty
+#-n returns if a var is set or not empty string
 if [ -n "$wpa_pid" ]; then
     #echo $wpa_pid
-    printf "Wpa_supplicant running on %d\n" $dhcp_pid
-    printf "killing %d\n" $wpa_pid
-    #pkill $wpa_pid
+    printf "Wpa_supplicant running on %d\n" $wpa_pid
+    printf "killing wpa_suppicant %d\n" $wpa_pid
+    kill $wpa_pid
 else
     #let error="cant find wpa process"
     printf "no wpa running\n"
 fi
 
 #let user chose which script
-#wpa_supplicant -i $interface -c /etc/wpa_supplcant/eduroam.conf
+#catch errors coming from wpa_supplicant
+printf "starting wpa_suplicant\n"
+wpa_supplicant -i $interface -c /etc/wpa_supplicant/$network.conf -B
 
 dhcp_pid="$(ps aux | pgrep dhcpcd)"
 
 if [ -n "$dhcp_pid" ]; then
-    printf "dhcpcd running on %d\n" $dhcp_pid
-    #printf "killing dhdpcd\n"
-    #pkill $dhcp_pid
+    printf "dhcpcd is running on %d\n" $dhcp_pid
 else
     printf "starting dhcpcd on %s\n" $interface
-    #dhcpcd $interface
+    dhcpcd $interface
+fi
+
+if [ "$1" = "lrz" ]; then
+    printf "starting vpn service...\n"
+    vpnc /etc/vpnc/lrz.conf
 fi
 
 exit 0
