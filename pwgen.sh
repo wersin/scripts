34be80b0f1baa893eeb742b99ccc29231f28ca57
mark <w3rsin@outlook.com>
2017-02-13 22:36:02 +0100

    it does what its supposed to do

--- pwgen.sh
+++ pwgen.sh
@@ -1,1 +1,31 @@
-cat /dev/urandom | tr -dc A-Za-z0-9_/%\*_=- | head -c9
+#!/bin/bash
+
+if [ "$1" = "-h" ]; then
+    printf "Usage: `basename "$0"` <length> [amt of passwords]\n"
+    printf "<length> the length of the password\n"
+    printf "to display this '-h'\n"
+    exit 1
+fi
+
+
+if [ -z "$1" ]; then
+    printf "please specify a password length\n"
+    exit 1
+fi
+#some magic to generate password
+specials="_/%&\^\@!*=\+-"
+#password="$(cat /dev/urandom | tr -dc A-Za-z0-9_/%\*_=- | head -c$1)"
+password="$(cat /dev/urandom | tr -dc A-Za-z0-9$specials | head -c$1)"
+
+printf "%s\n" "$password"
+
+if [ -n "$2" ]; then
+    for (( i=1; i < "$2"; i++ ))
+    do
+        password="$(cat /dev/urandom | tr -dc A-Za-z0-9$specials | head -c$1)"
+
+        printf "%s\n" "$password"
+    done
+fi
+
+exit 0
