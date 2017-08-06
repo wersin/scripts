f457c984b882f341c9a93f00ab3d1085dc2487a8
mark <w3rsin@outlook.com>
2017-08-06 20:04:06 +0200

    refactor pwgen

---
+++ pwgen.sh
@@ -0,0 +1,113 @@
+#!/bin/zsh
+
+usage()
+{
+    echo "---------------------------------"
+    echo "Program to generate random passwords using kernels random number generator."
+    echo "Default: generates one password of length 8"
+    echo "Usage:"
+    echo "-h, --help     Print this help message"
+    echo "-n, --amount	 Amount of passwords to generate"
+    echo "-l, --length   Length of passwords to generate"
+    echo "example: $1 -id 120 -f jpg -d my-folder"
+    echo "---------------------------------"
+}
+
+gen_pw()
+{
+	if [ -z ${1+x} ]; then
+		length = 8
+	else
+		length = $1
+	fi
+	#some magic to generate password
+	specials="_/%&\^\@!*=\+-"
+	#password="$(cat /dev/urandom | tr -dc A-Za-z0-9_/%\*_=- | head -c$1)"
+	password="$(cat /dev/urandom | tr -dc A-Za-z0-9$specials | head -c$length)"
+
+	printf "%s\n" "$password"
+	
+	#TODO: 	pass num as second argumen
+	if [ -n "$num" ]; then
+	    for (( i=1; i < $num; i++ ))
+	    do
+		password="$(cat /dev/urandom | tr -dc A-Za-z0-9$specials | head -c$length)"
+
+		printf "%s\n" "$password"
+	    done
+	fi
+
+}
+
+main()
+{
+    while [[ $# -gt 1 ]]
+    do
+        key="$1"
+
+        case $key in
+            -h|--help)
+                extension="$2"
+                shift # past argument
+            ;;
+            -d|--directory)
+                DIR="$2"
+                shift # past argument
+            ;;
+            -id)
+                id="$2"
+            shift # past argument
+            ;;
+            --default)
+            DEFAULT=YES
+            ;;
+            *)
+                    # unknown option
+            ;;
+        esac
+        shift # past argument or value
+    done
+}
+
+main "$@"
+
+
+#length=8
+#while getopts ':hl:n:' option; do
+#  case "$option" in
+#    h) echo "$usage"
+#       exit
+#       ;;
+#    l) length=$OPTARG
+#       ;;
+#    n) num=$OPTARG
+#       ;;
+#    :) printf "missing argument for -%s\n" "$OPTARG" >&2
+#       echo "$usage" >&2
+#       exit 1
+#       ;;
+#   \?) printf "illegal option: -%s\n" "$OPTARG" >&2
+#       echo "$usage" >&2
+#       exit 1
+#       ;;
+#  esac
+#done
+#shift $((OPTIND - 1))
+
+#some magic to generate password
+#specials="_/%&\^\@!*=\+-"
+##password="$(cat /dev/urandom | tr -dc A-Za-z0-9_/%\*_=- | head -c$1)"
+#password="$(cat /dev/urandom | tr -dc A-Za-z0-9$specials | head -c$length)"
+#
+#printf "%s\n" "$password"
+#
+#if [ -n "$num" ]; then
+#    for (( i=1; i < $num; i++ ))
+#    do
+#        password="$(cat /dev/urandom | tr -dc A-Za-z0-9$specials | head -c$length)"
+#
+#        printf "%s\n" "$password"
+#    done
+#fi
+#
+#exit 0
