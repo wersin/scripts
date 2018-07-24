f2334ff5c0ecec384084493f9d9d7d00530b900e
mark <mark@bley.com>
2018-07-24 19:48:01 +0200

    added info

--- set-keyboard.sh
+++ set-keyboard.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+#awk '{print $(NF-1), $NF;}' to print e.g. the last element and last-1
 layout=$(setxkbmap -print -verbose 10 | sed -n 9p | awk '{print $NF;}')
 
 if [ "$layout" = "de" ]; then
