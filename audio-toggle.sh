a3d754d6633fe2d10311b747ab0e2b1c4c30d7b8
mark <mark@bley.com>
2018-07-30 08:50:40 +0200

    audio toggle script, cause alsa daemon not handling right mute/unmute

---
+++ audio-toggle.sh
@@ -0,0 +1,14 @@
+#/bin/sh
+
+status=$(amixer | grep -A 25 Master | sed -n 5p | awk '{print $NF;}')
+volume=$(amixer | grep -A 25 Master | sed -n 5p | awk -F'[][]' '{print $2}')
+echo $volume
+
+if [ "$status" = "[on]" ]; then
+    amixer sset Master mute
+elif [ "$status" = "[off]" ]; then
+    alsactl restore
+    amixer sset Master unmute
+    amixer sset Master $volume
+fi
+
